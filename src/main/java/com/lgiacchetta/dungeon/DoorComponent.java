4a5515a44697e6e81640c901c344f5282b78d211
Luca Giacchetta <luke.jacket2001@gmail.com>
2023-06-16 18:20:59 +0200

    Added doors, damage effect Changed skin directory Minor changes on internal stuff

---
+++ src/main/java/com/lgiacchetta/dungeon/DoorComponent.java
@@ -0,0 +1,42 @@
+package com.lgiacchetta.dungeon;
+
+import com.almasb.fxgl.dsl.FXGL;
+import com.almasb.fxgl.entity.component.Component;
+import com.almasb.fxgl.physics.PhysicsComponent;
+import com.almasb.fxgl.texture.Texture;
+import javafx.scene.image.Image;
+public class DoorComponent extends Component {
+    private PhysicsComponent physics;
+    private Texture texture;
+    private final Image closed, open;
+    private final int connectedPlate;
+    public DoorComponent(int connectedPlate) {
+        this.connectedPlate = connectedPlate;
+        closed = FXGL.image("door/doors_leaf_closed.png");
+        open = FXGL.image("door/doors_leaf_open.png");
+
+        texture = new Texture(closed);
+    }
+    public void change() {
+        if (isOpen()) {
+            texture.setImage(closed);
+            physics.getBody().setActive(true);
+        } else {
+            texture.setImage(open);
+            physics.getBody().setActive(false);
+        }
+    }
+
+    public boolean isOpen() {
+        return texture.getImage() == open;
+    }
+
+    @Override
+    public void onAdded() {
+        entity.getViewComponent().addChild(texture);
+    }
+
+    public int getConnectedPlate() {
+        return connectedPlate;
+    }
+}
