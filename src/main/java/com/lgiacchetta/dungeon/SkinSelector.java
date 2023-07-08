b05894feed4d96df44b1ceddc272527c555f6200
Luca Giacchetta <luke.jacket2001@gmail.com>
2023-07-08 16:33:29 +0200

    Skin Selector change UI bug fix

--- src/main/java/com/lgiacchetta/dungeon/SkinSelector.java
+++ src/main/java/com/lgiacchetta/dungeon/SkinSelector.java
@@ -2,7 +2,7 @@ package com.lgiacchetta.dungeon;
 
 import com.almasb.fxgl.texture.AnimatedTexture;
 import javafx.beans.binding.Bindings;
-import javafx.beans.property.StringProperty;
+import javafx.beans.property.IntegerProperty;
 import javafx.geometry.Pos;
 import javafx.scene.layout.HBox;
 import javafx.scene.paint.Color;
@@ -13,44 +13,32 @@ import static com.lgiacchetta.dungeon.Utils.HEROES;
 import static com.lgiacchetta.dungeon.Utils.getAnimationChannel;
 
 public class SkinSelector extends HBox {
-    private final AnimatedTexture texture;
-    private final StringProperty asset;
-    private int index;
-
-    public SkinSelector(StringProperty asset) {
+    public SkinSelector(IntegerProperty skinIndex) {
         super(40.0);
-        this.asset = asset;
-        this.index = HEROES.indexOf(asset.get());
 
-        texture = new AnimatedTexture(getAnimationChannel(asset.get(), 4, 0.5));
+        AnimatedTexture texture = new AnimatedTexture(getAnimationChannel(HEROES.get(skinIndex.get()), 4, 0.5));
         texture.loop();
         texture.setScaleX(3.0);
         texture.setScaleY(3.0);
         getSceneService().getCurrentScene().addListener(texture);
 
         Polygon rightArrow = new Polygon(0.0, 0.0, 0.0, 20.0, 20.0, 10.0);
-        rightArrow.fillProperty().bind(
-                Bindings.when(rightArrow.hoverProperty()).then(Color.GREEN).otherwise(Color.YELLOW));
-        rightArrow.setOnMouseClicked(event -> next());
+        rightArrow.fillProperty().bind(Bindings.when(rightArrow.hoverProperty()).then(Color.GREEN).otherwise(Color.YELLOW));
+        rightArrow.setOnMouseClicked(event -> {
+            skinIndex.set(skinIndex.get() + 1);
+            if (skinIndex.greaterThanOrEqualTo(HEROES.size()).get()) skinIndex.set(0);
+            texture.loopAnimationChannel(getAnimationChannel(HEROES.get(skinIndex.get()), 4, 0.5));
+        });
 
         Polygon leftArrow = new Polygon(20.0, 0.0, 20.0, 20.0, 0.0, 10.0);
-        leftArrow.fillProperty().bind(
-                Bindings.when(leftArrow.hoverProperty()).then(Color.GREEN).otherwise(Color.YELLOW));
-        leftArrow.setOnMouseClicked(event -> prev());
+        leftArrow.fillProperty().bind(Bindings.when(leftArrow.hoverProperty()).then(Color.GREEN).otherwise(Color.YELLOW));
+        leftArrow.setOnMouseClicked(event -> {
+            skinIndex.set(skinIndex.get() - 1);
+            if (skinIndex.lessThan(0).get()) skinIndex.set(HEROES.size() - 1);
+            texture.loopAnimationChannel(getAnimationChannel(HEROES.get(skinIndex.get()), 4, 0.5));
+        });
 
         getChildren().addAll(leftArrow, texture, rightArrow);
         setAlignment(Pos.CENTER);
-    }
-
-    private void next() {
-        index = (index + 1) % HEROES.size();
-        asset.set(HEROES.get(index));
-        texture.loopAnimationChannel(getAnimationChannel(asset.get(), 4, 0.5));
-    }
-
-    private void prev() {
-        index = (index + HEROES.size() - 1) % HEROES.size();
-        asset.set(HEROES.get(index));
-        texture.loopAnimationChannel(getAnimationChannel(asset.get(), 4, 0.5));
     }
 }
